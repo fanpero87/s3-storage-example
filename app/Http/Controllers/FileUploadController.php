2a3a0244acb3d0087f21c754ac721fe916383c0b
Fabio Perez <fabio@fongo.com>
2025-01-02 18:21:43 +0000

    changes to controller

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,13 +10,31 @@ class FileUploadController extends Controller
 {
     public function store(Request $request)
     {
+        /**
+         * Code to Store files on AWS.
+         */
+        // $image = $request->file('uploadedfile');
+        // $fileName = 'uploaded' . '-' . Carbon::now()->format('d-m-Y');
+        // $filePath = '/Images/' . $fileName;
+
+        // Storage::disk('s3')->put($filePath, $image);
+
+        // return redirect('/')->with('message', 'File uploded successfully!');
+
+        /**
+         * Code to store files Locally.
+         */
         $image = $request->file('uploadedfile');
-        $fileName = 'uploaded' . '-' . Carbon::now()->format('d-m-Y');
-        $filePath = '/Images/' . $fileName;
+        $oringalName = $request->file('uploadedfile')->getClientOriginalName();
+        $extension = $request->file('uploadedfile')->getClientOriginalExtension();
+        $size = $request->file('uploadedfile')->getSize();
+        $uploadTime = $request->file('uploadedfile')->getaTime();
+        $fileDate = Carbon::now()->format('d-m-Y');
+        $fileNameToStore = $oringalName . '_' . time() . '.' . $extension;
+        $filePath = '/Images/';
+        dd($image, time());
 
-        Storage::disk('s3')->put($filePath, $image);
-
+        Storage::disk('local')->put($filePath, $image);
         return redirect('/')->with('message', 'File uploded successfully!');
-        dd('message');
     }
 }
